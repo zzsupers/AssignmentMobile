cb1eaf451bf3be3767468127671950e27afa3704
wakiki0123 <luv92no1@gmail.com>
2019-05-16 21:35:03 +0700

    Code of Khoi

---
+++ app/src/main/java/com/example/nguyentran/docbao/MainActivity/WebSiteObject.java
@@ -0,0 +1,15 @@
+package com.example.nguyentran.docbao.MainActivity;
+
+/**
+ * Created by nguyentran on 01/04/2019.
+ */
+
+public class WebSiteObject {
+    public int image;
+    public String tenMenu;
+
+    public WebSiteObject(int image, String tenMenu) {
+        this.image = image;
+        this.tenMenu = tenMenu;
+    }
+}
